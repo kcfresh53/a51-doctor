d7ee183278d4e575b71f80e92d8ccd5c3b2d045d
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2024-11-15 20:21:07 -0500

    Modified stamper script and added stamp timer

--- scripts/start_zone.gd
+++ scripts/start_zone.gd
@@ -1,14 +1,27 @@
 extends Node3D
 
 
-func _ready() -> void:
-	pass
-
-
-func _process(delta: float) -> void:
-	pass
+var stamperPickedUp = false
 
 
 func _on_document_area_entered(area: Area3D) -> void:
+	if (area.collision_layer == 6 and stamperPickedUp):
+		$StampTimer.start()
+
+
+func _on_document_area_exited(area: Area3D) -> void:
 	if (area.collision_layer == 6):
-		print("Stamped")
+		$StampTimer.stop()
+
+
+func _on_stamp_timer_timeout() -> void:
+	print("Stamped")
+
+
+func _on_stamper_picked_up(pickable: Variant) -> void:
+	stamperPickedUp = true
+
+
+func _on_stamper_dropped(pickable: Variant) -> void:
+	stamperPickedUp = false
+	$StampTimer.stop()
